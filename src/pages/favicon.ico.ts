244ac1f1e9cefebce2882f72941f37cd46010f0f
binzcodes <r@rbi.nz>
2024-10-23 19:57:34 +0100

    feat: update favicon, manifest

--- src/pages/favicon.ico.ts
+++ src/pages/favicon.ico.ts
@@ -1,20 +1,7 @@
 import type { APIRoute } from "astro";
-import sharp from "sharp";
-import ico from "sharp-ico";
+import Favicon from "@binz/visor/src/lib/favicon"
 import path from "node:path";
+
 const faviconSrc = path.resolve("src/images/Logo.svg");
 
-export const GET: APIRoute = async () => {
-  // generate 32x32 png
-  const buffer32 = await sharp(faviconSrc)
-    .resize(32)
-    .toFormat("png")
-    .toBuffer();
-
-  // generate ico
-  const icoBuffer = ico.encode([buffer32]);
-
-  return new Response(icoBuffer, {
-    headers: { "Content-Type": "image/x-icon" },
-  });
-};
+export const GET: APIRoute = Favicon({ faviconSrc });
